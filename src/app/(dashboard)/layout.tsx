db4ec5ef67df7c63828e7a3a62dec10c10a75643
kevinthbrt <kevin.thubert@gmail.com>
2026-02-05 19:59:36 +0100

    Add payment method editor for consultations

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -3,6 +3,8 @@ import { createClient } from '@/lib/db/server'
 import { Sidebar } from '@/components/layout/sidebar'
 import { Header } from '@/components/layout/header'
 
+export const dynamic = 'force-dynamic'
+
 export default async function DashboardLayout({
   children,
 }: {
